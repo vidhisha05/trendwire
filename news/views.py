26cbc83e48b7d8e00d3a805e404033d4974f9bb3
Nirogi Padma Venkata Mallika <npadma_csd216742@mgit.ac.in>
2025-03-10 15:41:04 +0530

    api collection done

--- news/views.py
+++ news/views.py
@@ -1,19 +1,20 @@
-
+import re
 import feedparser
 from django.shortcuts import render
 
 def google_news_feed(request):
     rss_url = "https://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN:en"
     feed = feedparser.parse(rss_url)
-
-    articles = [
-        {
-            "title": entry.title,
-            "link": entry.link,
-            "published": entry.published,
-            "summary": entry.summary,
-        }
-        for entry in feed.entries[:10]
-    ]
+    articles =[]
+    for entry in feed.entries[:10]:
+        clean_summary = re.sub(r"<[^>]*>", "", entry.summary) 
+        articles.append(
+            {
+                "title": entry.title,
+                "link": entry.link,
+                "published": entry.published,
+                "summary": clean_summary,
+            }
+        )
 
     return render(request, "news_feed.html", {"articles": articles})
